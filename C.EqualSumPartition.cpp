37e9cdb96e35afb4753b3de183cd6518d276593e
Prateek Ravi <71365724+PrateekRavi301@users.noreply.github.com>
2022-02-27 19:22:18 +0530

    Update C.EqualSumPartition.cpp

--- C.EqualSumPartition.cpp
+++ C.EqualSumPartition.cpp
@@ -26,8 +26,8 @@ int main()
 		}
 	}
 	
-	for(int i=1;i<=n+1;i++){
-		for(int j=1;j<=sum+1;j++){
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<sum+1;j++){
 			if(a[i-1]<=j){
 				 dp[i][j] = dp[i-1][j-a[i-1]] || dp[i-1][j] ;
 			}else if(a[i-1]>j){
