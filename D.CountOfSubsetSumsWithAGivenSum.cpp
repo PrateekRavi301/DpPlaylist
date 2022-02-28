e5ae5730634ba67a53714f32f02f40b391552b7f
Prateek Ravi <71365724+PrateekRavi301@users.noreply.github.com>
2022-02-28 12:49:58 +0530

    Create D.CountOfSubsetSumsWithAGivenSum.cpp

---
+++ D.CountOfSubsetSumsWithAGivenSum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std ;
+typedef pair<int,int> pp ;
+
+
+
+int main()
+{
+	int n=5,sum=10;
+//	cin>>n>>w;
+	int a[n] = {2,3,7,8,10};
+//	for(int i=0;i<n;i++)cin>>a[i];
+
+	int dp[n+1][sum+1];
+	
+	for(int i=0;i<n+1;i++){
+		for(int j=0;j<sum+1;j++){
+			if(i==0)dp[i][j]=0;
+			if(j==0)dp[i][j]=1;
+		}
+	}
+	
+	for(int i=1;i<n+1;i++){
+		for(int j=1;j<sum+1;j++){
+			if(a[i-1]<=j){
+				 dp[i][j] = dp[i-1][j-a[i-1]] + dp[i-1][j] ;
+			}else if(a[i-1]>j){
+				 dp[i][j] = dp[i-1][j];
+			}
+		}
+	}
+	
+	cout<<dp[n][sum];
+	return 0;
+}
